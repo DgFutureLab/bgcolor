b747638657b1802f30b9df1f176a2478a279de3a
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-06-12 14:16:56 +0900

    Moves hardware related files into subdir

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,5 @@ flapp = Flask(__name__)
 from flask.ext.socketio import SocketIO
 socketio = SocketIO(flapp)
 
-def flaskify(path):
-	return path.replace('app', '')
-
 flapp.flaskify = flaskify
 from app import views, conf
