6800aef337e996cca002d5b5151449730567f2dd
Halfdan Rump <halfdan-rump@garage.co.jp>
2014-06-12 14:36:58 +0900

    Run scan_client.py on PC with serial port and run.py on webserver

--- app/__init__.py
+++ app/__init__.py
@@ -4,5 +4,4 @@ flapp = Flask(__name__)
 from flask.ext.socketio import SocketIO
 socketio = SocketIO(flapp)
 
-flapp.flaskify = flaskify
 from app import views, conf
--- run.py
+++ run.py
@@ -1,32 +1,8 @@
 from app import flapp, socketio
-from threading import Thread
+# from threading import Thread
 
-import os
-import shutil
-from random import sample, gauss
-import time
-import argparse
-from app import rcon
-from random import sample
-import serial
-import re
-
-
-
-def read_serial_port():
-	
-	device = '/dev/' + filter(lambda x: re.match('tty.usbmodem*', x), os.listdir('/dev'))[0]
-	serial_connection = serial.Serial(device, 9600)
-	
-	while True:
-		reading = serial_connection.readline()
-		reading = int((int(reading) / 1024.0) * 255)
-		rgb = (reading, 128, 64)
-		color = '#'+''.join(map(chr, rgb)).encode('hex')
-		print 'From serial:', color
-		socketio.emit('new serial data', {'color':color}, namespace = '/serial')
-		# time.sleep(0.1)
 
 if __name__ == "__main__":
-	Thread(target = read_serial_port).start()
+	# Thread(target = read_serial_port).start()
+	flapp.config['DEBUG'] = True
 	socketio.run(flapp, port = 8080)
